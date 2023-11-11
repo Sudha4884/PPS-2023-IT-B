684bec492dead1d9b630e0774de8180ca71ac106
23wh1a1280 <150422029+23wh1a1280@users.noreply.github.com>
2023-11-11 21:30:47 +0530

    Add files via upload

---
+++ 23WH1A1280/Arithmetic Operations.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+     int a,b;
+     printf("\n Enter the values of a and b");
+     scanf("%d%d",&a,&b);
+     //Arithmetic operations;
+     printf("\n Sum of two numbers is %d",a+b);
+     printf("\n Difference of two numbers is %d",a-b);
+     printf("\n Product of two numbers is %d",a*b);
+     printf("\n Division of two numbers is %d",a/b);
+     printf("\n Remainder of two numbers when divided is %d",a%b);
+     return 0;
+}
+
+ 
+ 
