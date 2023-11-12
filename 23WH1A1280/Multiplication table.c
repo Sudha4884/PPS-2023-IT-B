ec94b4d7b69bc83cd28b976af0faa06a93694541
23wh1a1280 <150422029+23wh1a1280@users.noreply.github.com>
2023-11-12 11:24:54 +0530

    Add files via upload

---
+++ 23WH1A1280/Multiplication table.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+//To print the multiplication table
+     int n,i;
+     printf("Enter the value of n");
+     scanf("%d",&n);
+     for(i=1;i<=10;i++){
+        printf("%d × %d = %d\n",n,i,n*i);
+     }
+ }       
+     
