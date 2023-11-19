7ab74cc7f48e227f22833edc146b9bf1ac0a543d
23wh1a1277 <150413264+23wh1a1277@users.noreply.github.com>
2023-11-19 12:30:34 -0800

    maximum and minimum of matrix arrays

--- 23WH1A1277/max_array.c
+++ 23WH1A1277/max_array.c
@@ -1,27 +1,42 @@
 #include<stdio.h>
-#include<string.h>
 int main()
 {
-    int a[10][10],r,c,i,j,max=0;
-    printf(" enter numbers of rows: ");
-    scanf("%d",&r);
-    printf(" enter numbers of coloums: ");
-    scanf("%d",&c);
-    printf("enter element number : ");
-    for(i=0; i<r; i++)
-    { for (j=0; j<c; j++)
-      {
-        scanf("%d",&a[i][j]);
-      }
+     int A[3][3],B[3][3],i,j,r1,r2,c1,c2;
+    printf("enter number of rows and columns: ");
+    scanf("%d%d",&r1,&c1);
+    printf("enter number of rows and columns: ");
+    scanf("%d%d",&r2,&c2);
+    if (r1==c1 && r2==c2)
+    {
+        printf(" \n enter %d elements: ",r1*c1); 
+         for (i=0; i<r1; i++)
+            {for (j=0; j<c1; j++)
+            scanf("%d",&A[i][j]);
+            }
+        printf(" \n enter %d elements: ",r2*c2);
+        for (i=0; i<r2; i++)
+            {for (j=0; j<c2; j++)
+            scanf("%d",&B[i][j]);
+            }
+        for (i=0; i<r1; i++)
+             {for(j=0; j<c1; j++)
+                {if (A[i][j]>B[i][j])
+                {
+                    printf("A is MAX, B is MIN \n");
+                }
+                else
+                {
+                    printf("B is MAX, A is MIN \n");
+                }
+                }
+                printf("\n");
+            }
     }
-    printf("\n ********** \n");
-    for(i=0; i<r; i++)
-    { for (j=0; j<r; j++)
-        {
-            if (max<a[i][j])
-            {printf("%2d",a[i][j]);}
-        }
-        printf("\n");
-    }
+    else 
+        printf("not possible");
+    
+    printf("\n ******* \n");
+    
     return 0;
 }
+
