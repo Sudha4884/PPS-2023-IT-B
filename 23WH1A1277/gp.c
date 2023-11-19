ee8dc9e20a430d26d4ffcb5f59da44c74ecce716
23wh1a1277 <150413264+23wh1a1277@users.noreply.github.com>
2023-11-19 11:22:47 -0800

    idk

---
+++ 23WH1A1277/gp.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,b;
+    printf("enter a:");
+    scanf("%d",&a);
+    printf("enter b:");
+    scanf("%d",&b);
+    printf("%d",pow(a,b));
+    return 0;
+}
