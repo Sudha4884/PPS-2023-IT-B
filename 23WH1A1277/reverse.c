6512addca959c8eb3bdae2d0fc7d1b3387fcb46b
23wh1a1277 <150413264+23wh1a1277@users.noreply.github.com>
2023-11-19 20:54:16 -0800

    reverse of numbers

--- 23WH1A1277/reverse.c
+++ 23WH1A1277/reverse.c
@@ -1,1 +1,16 @@
-#inlcude<stdio.h>
+#include<stdio.h>
+void main()
+{
+    int r=0,d,n;
+    scanf("%d",&n);
+    
+    while (n!=0)
+    {
+    d = n%10;
+    r = r*10+d;
+    n = n/10;
+        
+    }
+    
+    printf("revverse no is %d",r);
+}
