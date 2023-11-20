0bdcaffbf20c24033d430702d3b28591a26ce8f9
23wh1a1277 <150413264+23wh1a1277@users.noreply.github.com>
2023-11-19 17:20:02 -0800

    multiplication table

---
+++ 23WH1A1277/table.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i,x,y;
+    printf("enter a number : ");
+    scanf("%d",&x);
+    y = 1;
+    for(i=1;i<11;i++)
+    {
+        printf("%d * %d = (%d) \n", x, i, y=x*i);
+    }
+}
