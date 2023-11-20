71a51802156d418c905d6db28dda8c979a305b21
GudimetlaMythili <150414999+GudimetlaMythili@users.noreply.github.com>
2023-11-20 15:57:43 +0530

    Add files via upload

---
+++ 23HW1A1268/max_min_array_ptr.c
@@ -0,0 +1,20 @@
+//To find the minimum and maximum of array using pointer
+void main(){
+int A[100],n,i,max,min,*ptr;
+ptr = A;
+scanf("%d",&n);
+for(int i=0; i<n; i++){
+        scanf("%d", A);
+    }
+    min=max=A[0];
+    for(int i=0; i<n; i++){
+        if(min < ptr + i){
+            min = ptr + i ;
+        }
+        if(max > ptr +i){
+            max = ptr + i;
+        }
+    }
+    printf("Maximum number is %d \n",max);
+    printf("Minimum number is %d \n",min);
+}
