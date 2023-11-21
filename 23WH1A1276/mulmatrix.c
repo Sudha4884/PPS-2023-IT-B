b7a39a8521a63e55620bf8f124cd958b802c8b67
MEGHANA77777 <150253601+MEGHANA77777@users.noreply.github.com>
2023-11-21 18:16:31 +0530

    Add files via upload

---
+++ 23WH1A1276/mulmatrix.c
@@ -0,0 +1,66 @@
+//MUTIPLICATION OF MATRIX
+#include<stdio.h>
+int main()
+{
+    int A[3][3],B[3][3],C[3][3],i,j,k;
+    printf("Enter the elements of matrix A:\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)                                                                                                                                                                                                                                                                                                                                                                                                        
+        {
+            scanf("%d",&A[i][j]);
+        }
+        
+    }
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+      {
+         printf(" %d ",A[i][j]);
+      }
+      printf("\n");
+    }
+    printf("Enter the elements of matrix B:");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&B[i][j]);
+        }
+        
+    }
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+      {
+         printf(" %d ",B[i][j]);
+      }
+      printf("\n");
+    }
+    printf("The matrix C is:");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            C[i][j] = 0;
+            
+            for(k=0;k<3;k++)
+            {
+                C[i][j] += A[i][k] * B[k][j] ;
+            }
+        }
+       
+    }
+
+    
+        for(i=0;i<3;i++)
+        {
+            for(j=0;j<3;j++)
+            {
+                printf("%d ",C[i][j]);
+            }
+            printf("\n");                                                                                                                                                                                                                                                                                                             
+        }
+        
+    return 0;
+}
