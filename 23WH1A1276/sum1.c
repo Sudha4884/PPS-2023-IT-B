d8b8cd00c65965cbb11d5802e09e7a72967471d0
MEGHANA77777 <150253601+MEGHANA77777@users.noreply.github.com>
2023-11-21 17:32:46 +0530

    Add files via upload

---
+++ 23WH1A1276/sum1.c
@@ -0,0 +1,33 @@
+//add the last and first digit of a number
+/*#include<stdio.h>
+int main()
+{
+    int num,l,f,sum;
+    printf("Enter a number:");
+    scanf("%d",&num);
+    l = num % 10;
+    while(num != 0)
+    {
+        f = num % 10;
+        num = num / 10;
+    }
+    sum = l + f;
+    printf("%d",sum);
+
+}*/
+
+/*#include<stdio.h>
+#include<math.h>
+void main()
+{
+    int rem,sum,num,rem1,n;
+    scanf("%d",&n);
+    printf("Enter a number:");
+    scanf("\n%d",&num);
+    rem = num % 10;
+    sum = 0;
+    sum = rem;
+    rem1 =  ((10^(n-1)) / num);
+    sum = sum + rem1;
+    printf("%d",sum);
+}*/
