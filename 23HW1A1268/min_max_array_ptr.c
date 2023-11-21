076da292b07148b9f13820d7f066450454066409
GudimetlaMythili <150414999+GudimetlaMythili@users.noreply.github.com>
2023-11-21 20:11:02 +0530

    Update and rename max_min_array_ptr.c to min_max_array_ptr.c

--- 23HW1A1268/min_max_array_ptr.c
+++ 23HW1A1268/min_max_array_ptr.c
@@ -1,18 +1,18 @@
-//To find the minimum and maximum of array using pointer
+///To find the minimum and maximum of array using pointer
 void main(){
 int A[100],n,i,max,min,*ptr;
 ptr = A;
 scanf("%d",&n);
 for(int i=0; i<n; i++){
-        scanf("%d", A);
+        scanf("%d", ptr+i);
     }
     min=max=A[0];
     for(int i=0; i<n; i++){
-        if(min < ptr + i){
-            min = ptr + i ;
+        if(min > *(ptr + i)){
+            min = *(ptr + i) ;
         }
-        if(max > ptr +i){
-            max = ptr + i;
+        if(max < *(ptr +i)){
+            max = *(ptr + i);
         }
     }
     printf("Maximum number is %d \n",max);
