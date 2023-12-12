4e2d378897d3d4c72508513b8fe85bebe66a5eaa
GudimetlaMythili <150414999+GudimetlaMythili@users.noreply.github.com>
2023-12-12 20:35:44 +0530

    Update primeno._range.c

--- 23HW1A1268/primeno._range.c
+++ 23HW1A1268/primeno._range.c
@@ -1,20 +1,25 @@
 // To find the prime nos. in a given range
 #include<stdio.h>
-void main(){
+void main()
+{
     int num,a,b, count = 0;
     printf("Enter the range to find prime numbers: ");
-    scanf("%d%d", &a,&b);
-    printf("Prime numbers in given range are: ");
-    for(num=1; num<=b; num++;){
-        if(num>=a && num<=b){
-            int i=1;
-            while(i<=num){
-                if(num % i == 0){
+    scanf("%d %d", &a,&b);
+    printf("Prime numbers in given range are: \n. ");
+    for(num=a; num<=b; num++)
+    {
+        int i=1;
+        while(i<=num)
+        {
+            if(num % i == 0){
                 count++;
-                }
-                i++;
             }
+            i++;
         }
+        if (count == 2)
+            {
+                printf("%d\n", num);
+            }
+        count = 0;
     }
-    printf("%d" , count) ;
 }
