bf674945b012ced30b029e392435cbb2ea9489b5
23wh1a12c1 <150234236+23wh1a12c1@users.noreply.github.com>
2023-12-13 17:29:16 +0530

    Add files via upload

--- 23WH1A12C1/read,print,arrayfun.c
+++ 23WH1A12C1/read,print,arrayfun.c
@@ -1,32 +1,31 @@
 #include<stdio.h>
-printArray(int x[],int n)
-void printArray(int x[],int n)
-void main()
-{
- int A[100],n,i;
- n=readArray(A);
- printArray(A,n);
-}
-void main()
-{
-  scanf("%d",&n);
-  for(i=0;i<n;i++)
-    scanf("%d",&A[i]);
-    printArray(A,n);
-}
-int readAray(int x[],int n)
-{
-  printf("enter n value: ");
-  scanf("%d",&n);
-  for(i=0;i<n;i++)
-   scanf("%d",&x[i]);
-}
-void printArray(int num[],int n)
-{
-  int i,num;
-  for(i=0;i<n;i++)
-  {
-    printf("%d",num[i]);
-  }
-}
+void printArray(int [],int );
+int readArray(int [],int );
 
+void main()
+{
+    int Arr[100];
+    int n;
+    int key;
+    n = readArray(Arr,n);
+    printArray(Arr, n);
+}
+int readArray(int x[], int n)
+{
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+    printf("Enter the elements of the array:\n");
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &x[i]);
+    }
+    return n;
+}
+void printArray(int x[], int n)
+{
+    printf("In print array: ");
+    for(int i = 0;i<n;i++)
+    {
+        printf("%d ",x[i]);
+    }
+}
