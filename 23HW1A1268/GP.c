d9f71396d9a0fb615a9eadde7c11833738d9ad27
GudimetlaMythili <150414999+GudimetlaMythili@users.noreply.github.com>
2023-12-25 15:09:08 +0530

    Update GP.c

--- 23HW1A1268/GP.c
+++ 23HW1A1268/GP.c
@@ -2,8 +2,13 @@
 #include<stdio.h>
 #include<math.h>
 int main(){
-	int x,n;
-	x=2; n=5;
- 	printf("%d",pow(x,n));
-	
+	int x,n,gp,i,res,sum=0;
+    printf("Enter x and n ");
+    scanf("%d %d",&x,&n);
+    for(i=0;i<=n;i++)
+    {
+        res = pow(x,i);
+ 	    sum += res;
+    }
+    printf("%d",sum);
 }
