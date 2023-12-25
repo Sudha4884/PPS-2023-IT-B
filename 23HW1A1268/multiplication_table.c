54034846d70fd9600616ae3c2ee863105eeb3372
GudimetlaMythili <150414999+GudimetlaMythili@users.noreply.github.com>
2023-12-25 13:07:39 +0530

    Update multiplication_table.c

--- 23HW1A1268/multiplication_table.c
+++ 23HW1A1268/multiplication_table.c
@@ -1,12 +1,16 @@
 //To print a multiplication table
 #include<stdio.h>
 void main(){
-    int n,i;
+    int n,i,r;
     printf("enter a number: ");
     scanf("%d",&n);
+    printf("Enter the number of rows: ");
+    scanf("%d",&r);
 
-    printf("The multiplication table of %d is", n);
-    for(i=0; i<=10; i++){
+    printf("The multiplication table of %d is \n", n);
+    for(i=1; i<=10; i++){
         printf("%d  x %d = %d \n", n,i,n*i);
+        if(i==r)
+            break;
     }
 }
