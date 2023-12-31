18d3462db28707c26deb76e77650645de30ea8d0
23wh1a1278 <150419170+23wh1a1278@users.noreply.github.com>
2023-12-31 20:56:30 +0530

    Create diagelements.c

---
+++ diagelements.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+    int A[3][3],r,c,i,j;
+    printf("Enter the rows and columns:");
+    scanf("%d %d",&r,&c);
+    printf("Enter the elements of A:");
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            scanf("\n%d",&A[i][j]);
+        }
+    }
+    for(i=0,j=0;j<c,i<r;j++,i++)
+    {
+        printf("%d ",A[i][j]);
+    }
+}
