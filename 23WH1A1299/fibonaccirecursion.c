9e9cd867c7b150f7bc945bcd27a4bdce71379eba
23wh1a1299 <150233142+23wh1a1299@users.noreply.github.com>
2024-01-02 18:51:27 +0530

    Add files via upload

---
+++ 23WH1A1299/fibonaccirecursion.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	printf("%d",fibonacci(7));
+}
+int fibonacci(int n)
+{
+	if(n==0)
+	return 0;
+	else if(n==1)
+	return 1;
+	return fibonacci(n-1)+fibonacci(n-2);
+}
