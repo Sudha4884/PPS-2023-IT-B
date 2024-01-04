3bd89e0efc92c3220b4eb64af4c9876d8d76bd17
ashwitha2728 <150237212+ashwitha2728@users.noreply.github.com>
2024-01-04 21:22:57 +0530

    Add files via upload

---
+++ 23WH1A12A3/decimal to binary ppt.c
@@ -0,0 +1,1 @@
+// decimal to binary#include<stdio.h>void main(){int B[100],n,i,j;printf("Enter n: ");scanf("%d",&n);i=0;while(n>0){B[i]=n%2;n=n/2;i++;}for (j=i-1;j>=0;j--)printf("%d",B[j]);}
