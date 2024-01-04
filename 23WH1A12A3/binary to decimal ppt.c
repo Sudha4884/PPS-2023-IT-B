fc7bc8bad3df60c0207d4bc47239e9d25ce84f2c
ashwitha2728 <150237212+ashwitha2728@users.noreply.github.com>
2024-01-04 21:20:02 +0530

    Add files via upload

---
+++ 23WH1A12A3/binary to decimal ppt.c
@@ -0,0 +1,1 @@
+// binary to decimal# include <stdio.h>      int main(){int decimal=0,binary,base=1,rem,num;printf("\n Enter the binary number: ");scanf("%d" ,&binary);num=binary;while(binary !=0){rem=binary%10;decimal=decimal+rem*base;binary=binary/10;base=base*2;}printf("decimal equivalent of the binary number %d is : %d ",num,decimal);return 0;}
