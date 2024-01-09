963beb812a405b8394892b45ca4e99efd163aa8b
adithireddy22 <150232434+adithireddy22@users.noreply.github.com>
2024-01-09 17:47:11 +0530

    Add files via upload

--- factorial.c
+++ factorial.c
@@ -1,12 +1,12 @@
-#include<stdio.h>
-void main()
-{
-printf("%d",factorial(9));
-}
-int factorial(int n)
-{
-if(n==0|| n==1)
-return 1;
-else 
-return n*factorial(n-1);
-}
+#include<stdio.h>  
+int main()    
+{    
+ int i,fact=1,number;    
+ printf("Enter a number: ");    
+  scanf("%d",&number);    
+    for(i=1;i<=number;i++){    
+      fact=fact*i;    
+  }    
+  printf("Factorial of %d is: %d",number,fact);    
+return 0;  
+}   
--- palindrome.c
+++ palindrome.c
@@ -1,43 +1,19 @@
-/*#include<stdio.h>
-int main()
-{
-    int n,num,rev,rem;
-    rem = 0;
-    rev = 0;
-    printf("Enter the number:");
-    scanf("%d\n",&num);
-    n = num;
-    while(num!=0)
-    {
-        rem = num % 10;
-        num /= 10;
-        rev = rev*10 + rem;
-        //printf("the numbers are %d ",rev);
-    }
-    printf("The reverse of  %d is %d",rev);
-    return 0;
-}*/
-
-
-//palindrome of a string
-#include<stdio.h>
-#include<string.h>
-int main()
-{
-char str[10];
-printf("Enter a string:\n");
-scanf("%s",&str);
-int l = 0;
-int h = strlen(str)-1;
-{
-    while(h > 1)
-    {
-        if (str[l++] != str[h--])
-        {
-            printf("The string is not a palindrome");
-            return 0;
-        }
-        printf("The string is a palindrome");
-    }
-}
-}
+#include<stdio.h>  
+int main()    
+{    
+int n,r,sum=0,temp;    
+printf("enter the number=");    
+scanf("%d",&n);    
+temp=n;    
+while(n>0)    
+{    
+r=n%10;    
+sum=(sum*10)+r;    
+n=n/10;    
+}    
+if(temp==sum)    
+printf("palindrome number ");    
+else    
+printf("not palindrome");   
+return 0;  
+}   
