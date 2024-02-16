187a90f56a399b4f76481b73dbadd421da4c737e
GudimetlaMythili <150414999+GudimetlaMythili@users.noreply.github.com>
2024-02-16 14:32:48 +0530

    Update perfectNO.py

--- 23HW1A1268/Python/perfectNO.py
+++ 23HW1A1268/Python/perfectNO.py
@@ -1,1 +1,10 @@
-
+#perfect number
+num = int(input())
+sum = 0
+for i in range(1,num):
+    if num % i == 0:
+        sum += i
+if sum == num:
+    print("Perfect number")
+else:
+    print("Not perfect number")
