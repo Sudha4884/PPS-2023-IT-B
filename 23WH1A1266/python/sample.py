c03f33c8d07c89fae5bf9456301eb05087ddd54c
lucasratna <150419301+lucasratna@users.noreply.github.com>
2024-02-16 14:11:03 +0530

    Update sample.py

--- 23WH1A1266/python/sample.py
+++ 23WH1A1266/python/sample.py
@@ -1,1 +1,17 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
+def print_primes_less_than(n):
+    print("Prime numbers less than", n, "are:")
+    for i in range(2, n):
+        if is_prime(i):
+            print(i, end=" ")
+
+# Example usage:
+given_number = 30
+print_primes_less_than(given_number)
