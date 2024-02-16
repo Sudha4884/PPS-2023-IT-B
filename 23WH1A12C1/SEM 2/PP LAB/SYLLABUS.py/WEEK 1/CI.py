db4d30d1dd5beca67e76ff26ca69765c3656607a
23wh1a12c1 <150234236+23wh1a12c1@users.noreply.github.com>
2024-02-16 16:31:57 +0530

    Update and rename C.I to CI.py

---
+++ 23WH1A12C1/SEM 2/PP LAB/SYLLABUS.py/WEEK 1/CI.py
@@ -0,0 +1,7 @@
+# Read name, address, email and phone number of a person
+# through keyboard and print the details.
+name=str(input("enter your name: "))
+address=input("enter your address: ")
+email=input("give your email id: ")
+phone_number=input("enter your phone number: ")
+print()
