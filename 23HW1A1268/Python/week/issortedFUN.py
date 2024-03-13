a07f47984ffce453b7da27d1b20957da3d1c4ca0
GudimetlaMythili <150414999+GudimetlaMythili@users.noreply.github.com>
2024-03-13 13:35:15 +0530

    Update issortedFUN.py

--- 23HW1A1268/Python/week/issortedFUN.py
+++ 23HW1A1268/Python/week/issortedFUN.py
@@ -20,3 +20,26 @@ def issorted(L):
 
 li = [int(i) for i in input().split()]
 print(issorted(li))
+
+#method 3
+import numpy as np
+def issorted(L):
+    return all(np.diff(L) >= 0)
+
+li = [int(i) for i in input().split()]
+if(issorted(li)) :
+    print("is sorted")
+else:
+    print("it is not sorted")
+
+#method 4
+import numpy as np
+def issorted(L):
+    List =  L
+    if all(np.sort(List) == L):
+        return True
+    else:
+        return False
+
+li = [int(i) for i in input().split()]
+print(issorted(li))
