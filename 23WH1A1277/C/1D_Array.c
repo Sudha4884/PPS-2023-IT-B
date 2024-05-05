d13e0403abc713b47bcd4182dd7fad51b2f9a9cc
23wh1a1277 <150413264+23wh1a1277@users.noreply.github.com>
2024-05-05 18:31:58 +0530

    1D_Array

--- 23WH1A1277/C/1D_Array.c
+++ 23WH1A1277/C/1D_Array.c
@@ -1,1 +1,17 @@
-
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int a[100],i;
+    printf(" enter numbers : ");
+    for(i=0; i<5; i++)
+    { 
+        scanf("%d",&a[i]);
+    }
+    printf("\n ********** \n ");
+    for(i=0; i<5; i++)
+    {
+        printf("%2d",a[i]);
+    }
+    return 0;
+}
